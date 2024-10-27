848b7711b28d102ae7483158d092200ad11f2ad7
nick brian <nbjm1234@outlook.com>
2024-10-26 21:08:19 -0600

    add guard to  validate existences of   foreign key user

---
+++ apps/tasks-manager-api-gateway/src/guards/validate-foreign-key-guard.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  BadRequestException,
+} from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { catchError } from 'rxjs/operators';
+import { lastValueFrom } from 'rxjs';
+import { HandlerMicroServiceErrors } from '../utils/custom-error-handler';
+
+@Injectable()
+export class ValidateForeignKey implements CanActivate {
+  constructor(
+    private usersService: UsersService,
+    private microserviceErrorHandler: HandlerMicroServiceErrors,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { userId } = request.body;
+
+    if (!userId) {
+      throw new BadRequestException('User ID not provided in request body');
+    }
+
+    try {
+      const user = await lastValueFrom(
+        this.usersService.getUserById(userId).pipe(
+          catchError((err) => {
+            throw this.microserviceErrorHandler.handleError(err);
+          }),
+        ),
+      );
+
+      if (!user.data) {
+        throw new BadRequestException('User not found');
+      }
+
+      return true;
+    } catch (error) {
+      throw new BadRequestException('Failed to validate user');
+    }
+  }
+}
--- apps/tasks-manager-api-gateway/src/tasks/tasks.controller.ts
+++ apps/tasks-manager-api-gateway/src/tasks/tasks.controller.ts
@@ -7,15 +7,18 @@ import {
   Param,
   Delete,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto, UpdateTaskDto } from './dto';
+import { ValidateForeignKey } from '../guards/validate-foreign-key-guard';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
+  @UseGuards(ValidateForeignKey)
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(createTaskDto);
   }
@@ -31,6 +34,7 @@ export class TasksController {
   }
 
   @Put(':id')
+  @UseGuards(ValidateForeignKey)
   update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
     return this.tasksService.update(id, updateTaskDto);
   }
