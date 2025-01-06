54f5ebee921c84838db3479bcb6084cb6f07a726
nick brian <nbjm1234@outlook.com>
2025-01-05 22:11:07 -0600

    add proto and start migration to grpc

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,24 +1,16 @@
+import { auth_package_name } from '@app/contracts/auth';
 import { NestFactory } from '@nestjs/core';
-import { AuthAppModule } from './auth-app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { v4 as uuid } from 'uuid';
-import { Partitioners } from 'kafkajs';
+import { join } from 'path';
+import { AuthAppModule } from './auth-app.module';
 async function bootstrap() {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AuthAppModule,
     {
-      transport: Transport.KAFKA,
+      transport: Transport.GRPC,
       options: {
-        client: {
-          clientId: `auth-${uuid()}`,
-          brokers: ['localhost:9092'],
-        },
-        consumer: {
-          groupId: 'auth-microservice-group',
-        },
-        producer: {
-          createPartitioner: Partitioners.LegacyPartitioner,
-        },
+        protoPath: join(__dirname, '../auth.proto'),
+        package: auth_package_name,
       },
     },
   );
--- libs/contracts/src/auth/index.ts
+++ libs/contracts/src/auth/index.ts
@@ -1,3 +1,5 @@
+export * from './auth.package.name';
+export * from './auth.types';
 export * from './create-auth.dto';
 export * from './message.patterns';
 export * from './update-auth.dto';
